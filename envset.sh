a7e77e0ce6da0864927faccd1e64e4c70fce54ef
yhep <yhep@yheps-Mac-mini-3.local>
2026-07-27 22:59:09 +0200

    Fix envset.sh for mac

--- envset.sh
+++ envset.sh
@@ -1,11 +1,18 @@
 #! /bin/bash
 
-# When using CVMFS
-source /cvmfs/sft.cern.ch/lcg/views/LCG_104/x86_64-el9-gcc11-opt/setup.sh
+
+#! /bin/bash
+
+# When using CVMFS (no X11)
+# source /cvmfs/sft.cern.ch/lcg/views/LCG_102/arm64-mac12-clang131-opt/setup.sh
+
+# When using local ROOT (X11 activated)
+source /Users/yhep/root-6.36.00/install/bin/thisroot.sh
 
 export INSTALL_DIR_PATH=$PWD/install
 
-export PATH=$PATH:$INSTALL_DIR_PATH/lib64
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$INSTALL_DIR_PATH/lib64
-export DYLD_LIBRARY_PATH=$INSTALL_DIR_PATH/lib64
-export PYTHONPATH=$PYTHONPATH:$INSTALL_DIR_PATH/lib64
+export PATH=$PATH:$INSTALL_DIR_PATH/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$INSTALL_DIR_PATH/lib
+export DYLD_LIBRARY_PATH=$INSTALL_DIR_PATH/lib
+export PYTHONPATH=$PYTHONPATH:$INSTALL_DIR_PATH/lib
+# export YAMLPATH=/Users/Shared/cvmfs/sft.cern.ch/lcg/releases/yamlcpp/0.6.3-d05b2/arm64-mac15-clang170-opt/lib
